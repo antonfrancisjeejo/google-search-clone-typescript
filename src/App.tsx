ff619d6f342cf92c7f63e6213b2685f1f5aaa0e9
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2022-05-15 23:40:05 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import React, { useState, FC } from "react";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Routes from "./components/Routes";
+import React from "react";
+import "./App.css";
+import CustomRoutes from "./Routes";
 
-const App: FC = () => {
-  const [darkTheme, setDarkTheme] = useState<boolean>(false);
+function App() {
   return (
-    <div className={darkTheme ? "dark" : ""}>
-      <div className="dark:bg-gray-900 dark:text-gray-200 black">
-        {/* <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} /> */}
-        {/* <Header /> */}
-        <Routes />
-      </div>
+    <div>
+      <CustomRoutes />
     </div>
   );
-};
+}
 
 export default App;
